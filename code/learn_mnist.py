0dcbaa7ceb41f54befcc7a84de925ec607d98955
Paul Brodersen <paulbrodersen@gmail.com>
2018-10-10 15:59:51 +0100

    Plot weights of each hidden neuron to visible neurons / input features

--- code/learn_mnist.py
+++ code/learn_mnist.py
@@ -199,6 +199,21 @@ def make_diagnostic_plots(forward_pass_activities,
     fig.tight_layout()
     fig.savefig(fdir + "mean_stimulus_response.pdf")
     fig.savefig(fdir + "mean_stimulus_response.svg")
+
+    # --------------------------------------------------------------------------------
+    # weights of each hidden neuron to input features
+
+    backward_weights = layers[-1].backward_weights
+    reshaped = get_unblockedshaped(backward_weights, (28,28), (20, 20)) # TODO don't hardcode MNIST and 400 unit hidden layer
+
+    fig, ax = plt.subplots(1,1)
+    ax.imshow(reshaped, cmap='gray')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    fig.tight_layout()
+    fig.savefig(fdir + "backward_weights.pdf")
+    fig.savefig(fdir + "backward_weights.svg")
+
     # plt.ion()
     # plt.show()
     # raw_input("Press any key to close figures...")
