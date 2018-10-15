9ea62703d394ebe5b08fedd8497c802730a8cb87
Paul Brodersen <paulbrodersen@gmail.com>
2018-10-15 16:55:06 +0100

    Remove superfluous (commented) code

--- code/learn_mnist.py
+++ code/learn_mnist.py
@@ -153,7 +153,6 @@ def make_diagnostic_plots(forward_pass_activities,
 
     fig.tight_layout()
     fig.savefig(fdir + "weight_alignment.pdf")
-    # fig.savefig(fdir + "weight_alignment.png")
     fig.savefig(fdir + "weight_alignment.svg")
 
     # --------------------------------------------------------------------------------
