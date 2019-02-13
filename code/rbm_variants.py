642368bd0476a4042f3d5700f44b1c62d2cad41e
Paul Brodersen <paulbrodersen@gmail.com>
2019-02-13 10:48:26 +0000

    Add commented out second version of Gibbs sampling step (ending on push down of actvity to inputs)

--- code/rbm_variants.py
+++ code/rbm_variants.py
@@ -92,12 +92,22 @@ class RestrictedBoltzmannMachine(object):
         activities[0] = batch
 
         # run CD Gibbs sampling steps
+        # version 1)
         activities = self._forward_pass(activities, layers)
         activities_0 = deepcopy(activities)
         for ii in range(cd):
             activities = self._backward_pass(activities, layers)
             activities = self._forward_pass(activities, layers)
         activities_cd = deepcopy(activities)
+
+        # # run CD Gibbs sampling steps
+        # # version 2)
+        # for ii in range(cd):
+        #     activities = self._forward_pass(activities, layers)
+        #     if ii == 0:
+        #         activities_0 = deepcopy(activities)
+        #     activities = self._backward_pass(activities, layers)
+        # activities_cd = deepcopy(activities)
 
         return activities_0, activities_cd
 
